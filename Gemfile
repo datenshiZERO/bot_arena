d85933604b53226bd67d10f9714b70dc3449eb8e
Bryan Bibat <bry@bryanbibat.net>
2015-03-08 01:40:50 +0800

    Update gems, add mina-sidekiq

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 gem 'rails', '4.2.0'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
@@ -40,6 +39,7 @@ end
 
 group :development do
   gem 'mina'
+  gem 'mina-sidekiq'
   gem 'spring-commands-rspec'
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,9 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/pids"]
+
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
 
@@ -60,6 +63,7 @@ task :deploy => :environment do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
+    invoke :'sidekiq:quiet'
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -70,6 +74,7 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      invoke :'sidekiq:restart'
     end
   end
 end
