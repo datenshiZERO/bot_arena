4cc9bf31feaa25e8dd0314f55bc27429438a3c7e
Bryan Bibat <bry@bryanbibat.net>
2015-03-17 03:44:21 +0800

    Veterancy icons

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 module PagesHelper
   def xp_display(user)
     if user.max_level?
-      "MAX LVL"
+      "(MAX LVL)"
     else
       "(#{number_with_delimiter user.total_xp}/#{number_with_delimiter user.xp_for_next_level})"
     end
