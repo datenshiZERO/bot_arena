c26aabd61fcf6e316b631a3dc404e85b9e6f14a4
Bryan Bibat <bry@bryanbibat.net>
2015-04-01 20:18:27 +0800

    Reduce battle list

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,9 +1,9 @@
 class BattlesController < ApplicationController
   def index
-    @battles = Battle.order("updated_at desc").includes(:arena).page params[:page]
+    @battles = Battle.order("updated_at desc").where("updated_at > ?", Date.today - 3.days).includes(:arena).page params[:page]
   end
 
   def show
-    @battle = Battle.find(params[:id])
+    @battle = Battle.includes(:arena, unit_battle_outcomes: { unit: :user}).find(params[:id])
   end
 end
