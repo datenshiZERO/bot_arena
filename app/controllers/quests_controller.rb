2cfada762af9b7212828e8bf69e1cae01ab0e2ed
Bryan Bibat <bry@bryanbibat.net>
2015-04-07 02:58:39 +0800

    Quests WIP, finish Forest, Set default party from last raid, Hide monsters from uncompleted

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -10,8 +10,23 @@ class QuestsController < ApplicationController
     if current_user.unlocked_quest?(@quest)
       @raid = Raid.new(quest_id: @quest.stage)
       @units = current_user.units.where(fired: false, arena: nil).all
+      if current_user.raids.count > 0
+        last_raid = current_user.raids.last
+        @raid.unit_1_id = last_raid.unit_1_id
+        @raid.unit_1_front = last_raid.unit_1_front
+        @raid.unit_2_id = last_raid.unit_2_id
+        @raid.unit_2_front = last_raid.unit_2_front
+        @raid.unit_3_id = last_raid.unit_3_id
+        @raid.unit_3_front = last_raid.unit_3_front
+        @raid.unit_4_id = last_raid.unit_4_id
+        @raid.unit_4_front = last_raid.unit_4_front
+        @raid.unit_5_id = last_raid.unit_5_id
+        @raid.unit_5_front = last_raid.unit_5_front
+        @raid.unit_6_id = last_raid.unit_6_id
+        @raid.unit_6_front = last_raid.unit_6_front
+      end
     else
-      redirect_to quests_path, alert: "You have not unlocked this stage yet"
+      redirect_to quests_path, alert: "You have not unlocked this quest yet"
     end
   end
 end
--- app/models/quest.rb
+++ app/models/quest.rb
@@ -166,12 +166,12 @@ class Quest
       name: "Sewers - 10",
       stage: 10,
       encounters: Encounter.generate_encounters([
-        [1, :green_slime, 8],
-        [2, :red_slime, 4],
-        [3, :blue_slime, 2]
+        [1, :green_slime, 12],
+        [2, :red_slime, 6],
+        [3, :blue_slime, 3]
       ]),
-      xp_win: 100,
-      credits_win: 100,
+      xp_win: 200,
+      credits_win: 200,
       area_id: 0,
       unlocks: [ ]
     },
@@ -247,6 +247,200 @@ class Quest
       credits_win: 60,
       area_id: 1,
       unlocks: [ 16, 21 ]
+    },
+    {
+      name: "Forest - 6",
+      stage: 16,
+      encounters: Encounter.generate_encounters([
+        [1, :wolf, 4],
+        [1, :goblin_warrior, 2],
+        [2, :goblin_warrior, 4],
+        [2, :alpha_wolf, 2],
+        [3, :rabid_rabbit, 4],
+        [3, :alpha_wolf, 4]
+      ]),
+      xp_win: 75,
+      credits_win: 75,
+      area_id: 1,
+      unlocks: [ 17 ]
+    },
+    {
+      name: "Forest - 7",
+      stage: 17,
+      encounters: Encounter.generate_encounters([
+        [1, :giant_snake, 8],
+        [1, :goblin_warrior, 2],
+        [2, :wolf, 6],
+        [2, :alpha_wolf, 2],
+        [3, :goblin_warrior, 4],
+        [3, :alpha_wolf, 2],
+        [4, :goblin_berserker, 2]
+      ]),
+      xp_win: 100,
+      credits_win: 100,
+      area_id: 1,
+      unlocks: [ 18 ]
+    },
+    {
+      name: "Forest - 8",
+      stage: 18,
+      encounters: Encounter.generate_encounters([
+        [1, :alpha_wolf, 4],
+        [2, :goblin_berserker, 2],
+        [3, :alpha_wolf, 5],
+        [4, :goblin_berserker, 2]
+      ]),
+      xp_win: 150,
+      credits_win: 150,
+      area_id: 1,
+      unlocks: [ 19 ]
+    },
+    {
+      name: "Forest - 9",
+      stage: 19,
+      encounters: Encounter.generate_encounters([
+        [1, :giant_snake, 5],
+        [1, :goblin_warrior, 5],
+        [2, :wolf, 8],
+        [2, :alpha_wolf, 3],
+        [3, :goblin_warrior, 5],
+        [3, :alpha_wolf, 2],
+        [4, :goblin_berserker, 3]
+      ]),
+      xp_win: 200,
+      credits_win: 200,
+      area_id: 1,
+      unlocks: [ 20 ]
+    },
+    {
+      name: "Forest - 10",
+      stage: 20,
+      encounters: Encounter.generate_encounters([
+        [1, :goblin_warrior, 4],
+        [2, :alpha_wolf, 4],
+        [3, :goblin_berserker, 4],
+        [4, :green_dragon, 1]
+      ]),
+      xp_win: 500,
+      credits_win: 500,
+      area_id: 1,
+      unlocks: [ ]
+    },
+    {
+      name: "Desert - 1",
+      stage: 21,
+      encounters: Encounter.generate_encounters([
+        [1, :giant_scorpion, 5],
+        [2, :giant_scorpion, 5],
+        [3, :giant_scorpion, 5],
+        [3, :red_slime, 3]
+      ]),
+      xp_win: 50,
+      credits_win: 50,
+      area_id: 2,
+      unlocks: [ 22 ]
+    },
+    {
+      name: "Desert - 2",
+      stage: 22,
+      encounters: Encounter.generate_encounters([
+        [1, :red_slime, 3],
+        [2, :giant_scorpion, 3],
+        [3, :red_slime, 6],
+        [4, :giant_scorpion, 6],
+        [5, :red_slime, 9],
+        [6, :giant_scorpion, 9],
+        [7, :salamander, 3]
+      ]),
+      xp_win: 75,
+      credits_win: 75,
+      area_id: 2,
+      unlocks: [ 23 ]
+    },
+    {
+      name: "Desert - 3",
+      stage: 23,
+      encounters: Encounter.generate_encounters([
+        [1, :red_slime, 10],
+        [2, :giant_scorpion, 10],
+        [3, :red_slime, 10],
+        [4, :giant_scorpion, 10],
+        [5, :salamander, 5]
+      ]),
+      xp_win: 150,
+      credits_win: 150,
+      area_id: 2,
+      unlocks: [ 24 ]
+    },
+    {
+      name: "Desert - 4",
+      stage: 24,
+      encounters: Encounter.generate_encounters([
+        [1, :red_slime, 6],
+        [1, :giant_scorpion, 6],
+        [2, :red_slime, 6],
+        [2, :giant_scorpion, 6],
+        [3, :salamander, 6],
+        [4, :red_slime, 6],
+        [4, :giant_scorpion, 6],
+        [5, :fire_scorpion, 3]
+      ]),
+      xp_win: 300,
+      credits_win: 300,
+      area_id: 2,
+      unlocks: [ 25 ]
+    },
+    {
+      name: "Desert - 5",
+      stage: 25,
+      encounters: Encounter.generate_encounters([
+        [1, :red_slime, 6],
+        [1, :giant_scorpion, 6],
+        [2, :red_slime, 6],
+        [2, :salamander, 3],
+        [3, :red_slime, 6],
+        [3, :fire_scorpion, 3],
+        [4, :red_slime, 4],
+        [4, :magma_spider, 2]
+      ]),
+      xp_win: 500,
+      credits_win: 500,
+      area_id: 2,
+      unlocks: [ 26, 31 ]
+    },
+    {
+      name: "Desert - 6",
+      stage: 26,
+      encounters: Encounter.generate_encounters([
+        [1, :red_slime, 6],
+        [1, :giant_scorpion, 6],
+        [2, :red_slime, 6],
+        [2, :salamander, 3],
+        [3, :red_slime, 6],
+        [3, :fire_scorpion, 3],
+        [4, :red_slime, 4],
+        [4, :magma_spider, 2]
+      ]),
+      xp_win: 500,
+      credits_win: 500,
+      area_id: 2,
+      unlocks: [ 27 ]
+    },
+    {
+      name: "Haunted Castle - 1",
+      stage: 31,
+      encounters: Encounter.generate_encounters([
+        [1, :green_slime, 6],
+        [1, :giant_spider, 6],
+        [2, :giant_spider, 6],
+        [2, :skeleton, 3],
+        [3, :skeleton, 6],
+        [4, :evil_eye, 3]
+      ]),
+      xp_win: 300,
+      credits_win: 300,
+      area_id: 3,
+      unlocks: [ 32 ]
     }
   ]
 
